166124e263cfece572db28e7554b2fd7cef2bd02
Zsolt114 <44722931+Zsolt114@users.noreply.github.com>
2022-09-30 17:36:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
+
+
+# Prepare pacman
+sudo mkdir -p /root/.gnupg
+sudo pacman-key --init && sudo pacman-key --populate archlinux manjaro && sudo pacman-key --refresh-keys
+
 # Upgrade all
 
-sudo pacman -Syyu;
+sudo pacman -Syyu
 
-# Install Garuda Package Repository
 
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com;
-sudo pacman-key --lsign-key FBA220DFC880C036;
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
-sudo -- sh -c "echo '[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf";
 
 # Install official packages
 
-sudo pacman -S --noconfirm mysql pidgin plank openssh  wine tk libreoffice-fresh nethogs python-pip docker;
-sudo pacman -S --noconfirm vim synapse samba pamac-gtk steam;
-sudo pacman -S --noconfirm lutris;
-sudo pacman -S --noconfirm discord;
-sudo pacman -S --noconfirm obs-studio;
-sudo pacman -S --noconfirm virtualbox;
-sudo pacman -S --noconfirm easyeffects;
-sudo pacman -S --noconfirm winetricks;
-sudo pacman -S --noconfirm make;
-sudo pacman -S --noconfirm archlinux-keyring qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat;
+sudo pacman -S --noconfirm mysql pidgin plank openssh  wine tk libreoffice-fresh nethogs python-pip docker
+sudo pacman -S --noconfirm vim synapse samba pamac-gtk steam
+sudo pacman -S --noconfirm lutris
+sudo pacman -S --noconfirm discord
+sudo pacman -S --noconfirm obs-studio
+sudo pacman -S --noconfirm virtualbox
+sudo pacman -S --noconfirm easyeffects
+sudo pacman -S --noconfirm winetricks
+sudo pacman -S --noconfirm make
+sudo pacman -S --noconfirm archlinux-keyring qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
 
 # Install AUR
 
@@ -32,17 +32,24 @@ sudo pamac install --no-confirm vivaldi vivaldi-ffmpeg-codecs flatpak libpamac-f
 
 sudo flatpak install --noninteractive bottles;
 
+# Install Garuda Package Repository
+
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com;
+sudo pacman-key --lsign-key FBA220DFC880C036;
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
+sudo -- sh -c "echo '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf";
+
 
 
 # Install Garuda Install
-
+sudo pacman -Sy --noconfirm alacritty-git
+sudo pacman -Sy --noconfirm expect
 cd;
 mkdir git;
 cd git;
 git clone https://gitlab.com/garuda-linux/applications/garuda-gamer.git;
 git clone https://gitlab.com/garuda-linux/applications/garuda-libs.git;
-sudo pacman -S --noconfirm alacritty-git;
-sudo pacman -S --noconfirm expect;
 cd garuda-libs;
 makepkg;
 sudo pacman -U --noconfirm garuda-libs*any.pkg*;
@@ -50,3 +57,4 @@ cd ..;
 cd garuda-gamer;
 makepkg;
 sudo pacman -U --noconfirm garuda-gamer*any.pkg*;
+
