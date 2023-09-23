81c1cf92ed55230aa8cca4ba2e1dd34f0414a614
Zsolt114 <44722931+Zsolt114@users.noreply.github.com>
2023-09-23 17:40:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,3 +78,4 @@ sudo pacman -S --noconfirm snapd;
 sudo systemctl enable --now snapd.socket;
 sudo ln -s /var/lib/snapd/snap /snap;
 sudo snap install eclipse --classic;
+sudo snap install pycharm-community --classic;
