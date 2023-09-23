151fbc0702f2a51e2e5c04c3fd0ea03dc224bdd3
Zsolt114 <44722931+Zsolt114@users.noreply.github.com>
2023-09-23 16:37:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ sudo flatpak install --noninteractive bottles;
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com;
 sudo pacman-key --lsign-key 3056513887B78AEB;
-sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
 sudo -- sh -c "echo '[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf";
 sudo pacman -Sy && sudo powerpill -Su && paru -Su;
