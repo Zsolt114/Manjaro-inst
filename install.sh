8b608daaa5196a62e1ed3e4e9a6e77f8ba0eb1a6
Zsolt114 <44722931+Zsolt114@users.noreply.github.com>
2023-09-23 17:12:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,11 +35,18 @@ sudo flatpak install --noninteractive bottles;
 
 # Install Garuda Package Repository
 
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+sudo pacman -Sy && sudo powerpill -Su && paru -Su;
+# Install Garuda Package Repository
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com;
 sudo pacman-key --lsign-key 3056513887B78AEB;
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
 sudo -- sh -c "echo '[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf";
+pacman-key --init
 sudo pacman -Sy && sudo powerpill -Su && paru -Su;
 
 
